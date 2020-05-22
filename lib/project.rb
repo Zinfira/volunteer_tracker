f9fd63ecf16e607871b5e698e0af9d1477f0c496
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-22 11:30:28 -0700

    Pass test to return the id of the project after saving project

--- lib/project.rb
+++ lib/project.rb
@@ -12,7 +12,7 @@ class Project
     projects = []
     returned_projects.each() do |project|
       title = project.fetch("title")
-      id = project.fetch("id")
+      id = project.fetch("id").to_i
       projects.push(Project.new({:title => title, :id => id}))
     end
     projects
@@ -20,7 +20,7 @@ class Project
 
   def save
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
-    id = result.first().fetch("id").to_i
+    @id = result.first().fetch("id").to_i 
   end
 
   def ==(project_to_compare)
