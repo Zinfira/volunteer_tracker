da6345fe5f853e7f2f7b5a037621cf3345d52018
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-22 10:38:47 -0700

    Add method to pass test 'is empty to start'

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,25 +1,20 @@
 require "spec_helper"
 
-# describe Project do
-#   describe '#title' do
-#     it 'returns the project title' do
-#       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       expect(project.title).to eq 'Teaching Kids to Code'
-#     end
-#   end
+describe Project do
+  # describe '#title' do
+  #   it 'returns the project title' do
+  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+  #     expect(project.title).to eq 'Teaching Kids to Code'
+  #   end
+  # end
+  
 
-#   context '#id' do
-#     it 'returns the id of the project before saving project' do
-#       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       expect(project.id).to eq nil
-#     end
-
-#     it 'returns the id of the project after saving project' do
-#       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       project.save
-#       expect(project.id).to be_an_instance_of Integer
-#     end
-#   end
+  #   it 'returns the id of the project after saving project' do
+  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+  #     project.save
+  #     expect(project.id).to be_an_instance_of Integer
+  #   end
+  # end
 
 #   describe '#==' do
 #     it 'is the same project if two projects have the same title' do
@@ -29,10 +24,11 @@ require "spec_helper"
 #     end
 #   end
 
-#   context '.all' do
-#     it 'is empty to start' do
-#       expect(Project.all).to eq []
-#     end
+  context '.all' do
+    it 'is empty to start' do
+      expect(Project.all).to eq []
+    end
+  end
 
 #     it 'returns all projects' do
 #       project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
@@ -90,4 +86,4 @@ require "spec_helper"
 #       expect(Project.all).to eq []
 #     end
 #   end
-# end
+end
